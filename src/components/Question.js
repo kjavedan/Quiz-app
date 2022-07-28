13dd3dc965f3568cc226ff2ada271b1b0b35563d
khaled javedan <khaledjavedan76@gmail.com>
2022-07-28 10:20:41 +0430

    getting data from an api

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 
-export default function Question(){
-
+export default function Question(props){
+    console.log(props)
+    const allAnswers = props.incorrect_answers;
+    
     return(
         <div className="question">
-            <div className="question_title">How would one say goodbye in Spanish?</div>
+            <div className="question_title">{props.question}</div>
             <div className="answers-container">
                 <div className="answer">Adiós</div>
                 <div className="answer">Hola</div>
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,8 +3,28 @@ import Question from "./Question";
 
 export default function Quiz() {
 
+// making an api call to receive the data
+    React.useEffect(()=>{
+        fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+            .then(res => res.json())
+            .then(data => setData(data.results))
+        },[])
+        
+        // state for our question data
+    const [data, setData] = React.useState([])
 
-    // state for our question
+    // map over my question data and create question component
+    const questions = data.map(item => {
+        return(
+        <Question
+        question = {item.question}
+        correctAnswer = {item.correct_answer}
+        incorrectAnswers = {item.incorrect_answers}
+         />
+        )
+    })
+    console.log(questions)
+
 
 
     const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
@@ -16,11 +36,7 @@ export default function Quiz() {
         <div className="quiz">
             <img className='top-blob' src="../images/top-blob.png"></img>
             <div className="questions-container">
-                <Question />
-                <Question />
-                <Question />
-                <Question />
-                <Question />
+                {questions}
             <button className="check-answers" onClick={handleClick}>Check Answers</button>
             </div>
             <img className='bottom-blob' src='../images/bottom-blob.png'></img>
