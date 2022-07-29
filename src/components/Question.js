56a4ae12230e4e54141ec0c2beb99727c46d6f22
khaled javedan <khaledjavedan76@gmail.com>
2022-07-29 12:21:19 +0430

    working on the selecting the answer

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,32 +1,65 @@
+import { nanoid } from "nanoid";
 import React from "react";
 
-
 export default function Question(props){
+
+
     // creatin an array for all answers
-    const allAnswers = props.answers.split(',');
-
-    console.log('unshuffled :' + allAnswers)
-
+    const answers = props.answers.split(',');
     // Fisher Yates Shuffle algorithem for shffling our array
-   let currentIndex = allAnswers.length;
+   let currentIndex = answers.length;
    while(--currentIndex > 0){
-    let temp = allAnswers[currentIndex];
+    let temp = answers[currentIndex];
     let randomIndex = Math.floor(Math.random() * (currentIndex + 1));
-    allAnswers[currentIndex] = allAnswers[randomIndex];
-    allAnswers[randomIndex] = temp;
+    answers[currentIndex] = answers[randomIndex];
+    answers[randomIndex] = temp;
    }
-      console.log('shuffled :' + allAnswers)
-      console.log('-----------------------')
+
+   // set our state so we can have uninque property for each answer
+   function initiolizeState(){
+   const newAnswers = [] ;
+   for(let i = 0; i < answers.length; i++){
+    newAnswers.push({
+        isHeld : false,
+        body : answers[i],
+        id : nanoid()
+    })
+   }
+   return newAnswers
+}  
+
+    const [state, setState] = React.useState(initiolizeState())
+    
+//    const styles = {
+//     backgroundColor : isHeld ? "#D6DBF5" : "",
+//     border : isHeld ? 'none' : ""
+//    }
+
+   // map over state and create seperate answer
+   const answerElements = state.map(answer => {
+    return <div 
+    key = {answer.id}
+    className = {`answer ${answer.isHeld ? "held" : ""}`}
+    onClick = {()=> handleClick(answer.id,answer.isHeld)}
+    >{answer.body}</div>
+   })  
+
+   function handleClick(answerId, isHeld){
+    console.log(answerId)
+    console.log(isHeld)
+    // I want to toggle isHeld proprty of the answer when it's clicked 
+    // loop over the state
+    // if id matches
+    // spread all propty and overwrite isHeld
+    setState(prevState => prevState.map(item => item.id === answerId ? {...item, isHeld : !isHeld} : {...item, isHeld : false} ));
+   }
 
     return(
         <div className="question">
             <div className="question_title">{props.question}</div>
             <div className="answers-container">
-                <div className="answer">{allAnswers[0]}</div>
-                <div className="answer">{allAnswers[1]}</div>
-                <div className="answer">{allAnswers[2]}</div>
-                <div className="answer">{allAnswers[3]}</div>
+            {answerElements}
             </div>
         </div>
     )
-}
+}  
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,22 +1,26 @@
 import React from "react";
+import { nanoid } from 'nanoid'
+
 import Question from "./Question";
 
 export default function Quiz() {
 
-// making an api call to receive the data
-    React.useEffect(()=>{
+    // making an api call to receive the data
+    React.useEffect(()=>{ // the use effect hook runs last
         fetch('https://opentdb.com/api.php?amount=5&type=multiple')
             .then(res => res.json())
             .then(data => setData(data.results))
         },[])
         
-        // state for our question data
+    // state for our question data
     const [data, setData] = React.useState([])
 
     // map over my question data and create question component
-    const questions = data.map((item , index) => {
+    const questions = data.map((item) => {
         return(
         <Question
+        key = {nanoid()}
+        id = {nanoid()}
         question = {item.question}
         correctAnswer = {item.correct_answer}
         incorrectAnswers = {item.incorrect_answers}
